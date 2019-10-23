3fbac2f8b4548ae72c4c687f7d315e00f536f105
Jian Kai <jktan-dev@outlook.com>
2019-10-23 14:04:13 +0800

    Assigning separate update quality methods based on item name

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -5,110 +5,105 @@ class GildedRose
 
   def update_quality
     @items.each do |item|
-      # Case block 1: Using @itemGrade to classify items by name
-      @itemGrade = ""
+      # Linking Item to desired update_quality method based on item.name
       case item.name
-        # Classify Aged Brie as Aged
       when "Aged Brie"
-        @itemGrade = "Aged"
-        # Classify Backstage Pass as Expirable
+        update_aged_item_quality(item)
       when "Backstage passes to a TAFKAL80ETC concert"
-        @itemGrade = "Expirable"
-        # Classify Sulfuras as Legendary
+        update_expirable_item_quality(item)
       when "Sulfuras, Hand of Ragnaros"
-        @itemGrade = "Legendary"
-        # Conjured Items
+        # No Logic required as unlike conventional items, The legendary item does not degrade in terms of quality or sell_in
       when "Conjured"
-        @itemGrade = "Conjured"
-        # Others Classified as common
+        update_conjured_item_quality(item)
       else
-        @itemGrade = "Common"
+        update_normal_item_quality(item)
       end
+    end
+  end
 
-      # Case Block 2, Updating sell_in and quality based on @itemGrade
-      case @itemGrade
-        # Logic to Handle Aged Items
-      when "Aged"
-        # If item quality less than 50
-        if item.quality < 50
-          # Increase item quality by 1
-          item.quality = item.quality + 1
-        end
-        # Logic to Handle Expirable Items
-      when "Expirable"
-        # For expirable items ,first manipulate the quality based on sell_in value
-        if item.sell_in >= 11
-          item.quality = item.quality + 1
-        elsif item.sell_in < 11 && item.sell_in >= 6
-          item.quality = item.quality + 2
-        elsif item.sell_in < 6 && item.sell_in > 0
-          item.quality = item.quality + 3
-        else
-          item.quality = 0
-        end
+  # Method to update Aged Items
+  def update_aged_item_quality(item)
+    # If item quality less than 50
+    if item.quality < 50
+      # Increase item quality by 1
+      item.quality = item.quality + 1
+    end
+  end
 
-        # Then scale down to quality to a max of 50 if required
-        if item.quality > 50
-          item.quality = 50
-        end
+  # Method to update Expirable Items
+  def update_expirable_item_quality(item)
+    # For expirable items ,first manipulate the quality based on sell_in value
+    if item.sell_in >= 11
+      item.quality = item.quality + 1
+    elsif item.sell_in < 11 && item.sell_in >= 6
+      item.quality = item.quality + 2
+    elsif item.sell_in < 6 && item.sell_in > 0
+      item.quality = item.quality + 3
+    else
+      item.quality = 0
+    end
 
-        # Then Decrease sell_in by 1
-        item.sell_in = item.sell_in - 1
-        # Logic to Handle Legendary Items
-      when "Legendary"
-        # Unlike conventional items, The legendary item does not degrade in terms of quality or sell_in
+    # Then scale down to quality to a max of 50 if required
+    if item.quality > 50
+      item.quality = 50
+    end
 
-        # Logic To deal With Conjured Items
-      when "Conjured"
-        # First Decrease sell_in by 1
-        item.sell_in = item.sell_in - 1
-        # If sell_in less than 0
-        if item.sell_in < 0
-          # (and) If Quality More than or Equal to 2
-          if item.quality >= 4
-            # Decrease Item quality by 2
-            item.quality = item.quality - 4
-            # Else (Quality less than 2)
-          else
-            # Assign Quality to 0
-            item.quality = 0
-          end
-          # Else (sell_in More than 0)
-        else
-          # (and) If Quality More than or Equal to 1
-          if item.quality >= 2
-            # Decrease Item quality by 1
-            item.quality = item.quality - 2
-          else
-            # Assign Quality to 0
-            item.quality = 0
-          end
-        end
+    # Then Decrease sell_in by 1
+    item.sell_in = item.sell_in - 1
+  end
+
+  # Method to update Conjured Items
+  def update_conjured_item_quality(item)
+    # First Decrease sell_in by 1
+    item.sell_in = item.sell_in - 1
+    # If sell_in less than 0
+    if item.sell_in < 0
+      # (and) If Quality More than or Equal to 2
+      if item.quality >= 4
+        # Decrease Item quality by 2
+        item.quality = item.quality - 4
+        # Else (Quality less than 2)
       else
-        # First Decrease sell_in by 1
-        item.sell_in = item.sell_in - 1
-        # If sell_in less than 0
-        if item.sell_in < 0
-          # (and) If Quality More than or Equal to 2
-          if item.quality >= 2
-            # Decrease Item quality by 2
-            item.quality = item.quality - 2
-            # Else (Quality less than 2)
-          else
-            # Assign Quality to 0
-            item.quality = 0
-          end
-          # Else (sell_in More than 0)
-        else
-          # (and) If Quality More than or Equal to 1
-          if item.quality >= 1
-            # Decrease Item quality by 1
-            item.quality = item.quality - 1
-          else
-            # Assign Quality to 0
-            item.quality = 0
-          end
-        end
+        # Assign Quality to 0
+        item.quality = 0
+      end
+      # Else (sell_in More than 0)
+    else
+      # (and) If Quality More than or Equal to 1
+      if item.quality >= 2
+        # Decrease Item quality by 1
+        item.quality = item.quality - 2
+      else
+        # Assign Quality to 0
+        item.quality = 0
+      end
+    end
+  end
+
+  # Method to update normal Items
+  def update_normal_item_quality (item)
+    # First Decrease sell_in by 1
+    item.sell_in = item.sell_in - 1
+    # If sell_in less than 0
+    if item.sell_in < 0
+      # (and) If Quality More than or Equal to 2
+      if item.quality >= 2
+        # Decrease Item quality by 2
+        item.quality = item.quality - 2
+        # Else (Quality less than 2)
+      else
+        # Assign Quality to 0
+        item.quality = 0
+      end
+      # Else (sell_in More than 0)
+    else
+      # (and) If Quality More than or Equal to 1
+      if item.quality >= 1
+        # Decrease Item quality by 1
+        item.quality = item.quality - 1
+      else
+        # Assign Quality to 0
+        item.quality = 0
       end
     end
   end
